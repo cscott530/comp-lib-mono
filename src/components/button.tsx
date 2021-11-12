ba6c7926b036fc95ca49cd74164794e658dcea46
Chris Scott <cscott@insiderintelligence.com>
2021-11-12 07:56:59 -0500

    feat: Add styled.

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,9 +1,15 @@
 import * as React from 'react';
+import styled from "styled-components";
+
+const ButtonStyle = styled.button <{rounded: boolean }>`
+    border: 1px solid black;
+    border-radius: ${({rounded}) => rounded ? '10px' : 0};
+`
 
 export type ButtonProps = {
     rounded: boolean
 };
 
 export const Button = (props: ButtonProps) => {
-    return <button style={{borderRadius: props.rounded ? '10px' : 0, border: '1px solid black'}}>My button</button>
+    return <ButtonStyle rounded={props.rounded}>My button</ButtonStyle>
 }
