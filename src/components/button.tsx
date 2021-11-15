9608a7fdb629da4e575b51934038d0f5b557df43
Chris Scott <cscott@insiderintelligence.com>
2021-11-15 07:44:07 -0500

    Fix tests.

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import styled from "styled-components";
 
-const ButtonStyle = styled.button <{rounded: boolean }>`
+const ButtonStyle = styled.button <{rounded?: boolean }>`
     border: 1px solid black;
     border-radius: ${({rounded}) => rounded ? '10px' : 0};
 `
 
 export type ButtonProps = {
-    rounded: boolean
+    rounded?: boolean
 };
 
 export const Button = (props: ButtonProps) => {
